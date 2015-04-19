94bcf4201dc079260eb9dbf7fdf02cb667009c4b
tchaly.bethmaure@gmail.com <tchaly@tlse.univ-tlse1.fr>
2015-04-20 01:05:10 +0200

    Fonction ajout noeud OK

--- astar.py
+++ astar.py
@@ -7,41 +7,60 @@ def AEtoile(grille, noeud_source, noeud_arrive):
 	parent_de_noeud = {} # dictionnaire noeud parent du noeud
 	liste_ouverte = [] # noeuds étudiés
 	liste_fermee = [] # noeuds appartiennent potentiellement à la solution
-
+	print noeud_source, noeud_arrive
 	parent_de_noeud[noeud_source.get_hash()] = None
 	noeud_courant = noeud_source
-	while(noeud_courant != noeud_arrive):	
+	while(noeud_courant != noeud_arrive):
+		if noeud_courant == noeud_arrive:
+			break
+
 		if(not noeud_courant in liste_fermee):
 			# heuristique = d(source, noeud) + d(arrivée, noeud)
 			noeud_courant.cout = grille.distance(noeud_courant, noeud_source) + grille.distance(noeud_courant, noeud_arrive)
-			if(noeud_courant in liste_ouverte):
-				noeud_dans_liste = None	
-				priorite_dans_liste = None	
-				for priorite,noeud in liste_ouverte:
-					if noeud == noeud_courant:
-						noeud_dans_liste = noeud
-				if(noeud_courant.cout < noeud_dans_liste.cout):
-					liste_ouverte.remove((noeud_dans_liste.cout, noeud_dans_liste))
-			heappush(liste_ouverte, (noeud_courant.cout, noeud_courant))	
-
+			ajouter_noeud_a_liste_ouverte(noeud_courant, liste_ouverte)
+			for noeud_voisin in noeud_courant.voisins:
+				ajouter_noeud_a_liste_ouverte(noeud_voisin, liste_ouverte)
+			# faire même chose pour voisin de noeud coourant
 		if len(liste_ouverte) != 0:
 			priorite_meilleur_noeud, meilleur_noeud = heappop(liste_ouverte)
-			print liste_ouverte,len(liste_ouverte)
-			print meilleur_noeud
-			liste_ouverte.remove((meilleur_noeud.cout, meilleur_noeud))
 			liste_fermee.append(meilleur_noeud)
+			print "Liste ouvert : ", liste_ouverte
+			print "Liste fermée : ", liste_fermee
+			print "Noeud courant : ", noeud_courant
 			noeud_precedant = noeud_courant
 			noeud_courant = meilleur_noeud
 			parent_de_noeud[noeud_courant.get_hash()] = noeud_precedant
 		else:
 			return False
 		dessiner_grille_AEtoile(grille, liste_ouverte, liste_fermee, noeud_source, noeud_arrive)
+		raw_input()
 	return True
 
-def dessiner_grille_AEtoile(grille, case_a_etudier, case_etudiees, source, arrivee):
+def ajouter_noeud_a_liste_ouverte(noeud_a_ajouter, liste_ouverte):
+	dans_liste = False
+	noeud_dans_liste = None	
+	for priorite,noeud in liste_ouverte:
+			if noeud == noeud_a_ajouter:
+				noeud_dans_liste = noeud
+				dans_liste = True
+	if(dans_liste):		
+		if(noeud_a_ajouter.cout < noeud_dans_liste.cout):
+			liste_ouverte.remove((noeud_dans_liste.cout, noeud_dans_liste))
+			heappush(liste_ouverte, (noeud_a_ajouter.cout, noeud_a_ajouter))
+	else:
+		heappush(liste_ouverte, (noeud_a_ajouter.cout, noeud_a_ajouter))
+	return liste_ouverte
+
+def dessiner_grille_AEtoile(grille, liste_ouverte, case_etudiees, source, arrivee):
 	compte = 0
 	dessin = ""
-	for case in self.cases:
+	case_a_etudier = []
+
+	# on construit la liste des cases étudiées à partir de la liste ouverte
+	for priorite, case in liste_ouverte:
+		case_a_etudier.append(case)
+
+	for case in grille.cases:
 		if case == source:
 			dessin += "S"
 		elif case == arrivee:
@@ -55,7 +74,7 @@ def dessiner_grille_AEtoile(grille, case_a_etudier, case_etudiees, source, arriv
 		else:
 			dessin += " "
 
-		if compte%self.largeur == self.largeur-1:
+		if compte%grille.largeur == grille.largeur-1:
 			dessin += "\n"
 		compte+=1
 	print dessin
@@ -73,4 +92,4 @@ if __name__ == '__main__':
 			arrivee = case
 			break
 
-	AEtoile(g, source, arrivee)
+	print AEtoile(g, source, arrivee)
